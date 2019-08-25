f2bc64a0970ab4c0f01d9f217d07980171a2d7b9
szczepaniakmichal <36078683+szczepaniakmichal@users.noreply.github.com>
2019-08-25 18:05:53 +0200

    refactor code

--- reports/bestStreaks.js
+++ reports/bestStreaks.js
@@ -1,85 +1,85 @@
-var fs = require('fs');
-var DayDate = require('../src/DayDate');
-var Streaks = require('../src/Streaks');
-var Streak = require('../src/Streak');
-var genSpaces = require('../src/genSpaces.js');
+/* eslint-disable linebreak-style */
+const fs = require('fs');
+const DayDate = require('../src/DayDate');
+const Streaks = require('../src/Streaks');
+const Streak = require('../src/Streak');
 
+let habits = [];
+let max; // Length of the longest habit name
 
-var habits = [];
-var max; // Length of the longest habit name
+let aa = fs.readFile('./data.txt', 'utf-8', (e, d) => {
+    habits = d.split(/\n/);
 
-fs.readFile('./data.txt', 'utf-8', function(e, d) {
-  habits = d.split(/\n/);
+    let done = [];
+    const dates = {};
+    let times = 1;
 
-  var done = [];
-  var dates = {};
-  var times = 1;
+    for (let i = 0; i < habits.length; i++) {
+        if (!done.includes(habits[i].substring(0, habits[i].indexOf(',')))) {
+            done = done.concat([habits[i].substring(0, habits[i].indexOf(','))]);
+            const name = habits[i].substring(0, habits[i].indexOf(','));
 
-  for (var i = 0; i < habits.length; i++) {
-    if (!done.includes(habits[i].substring(0, habits[i].indexOf(',')))) {
-      done = done.concat([habits[i].substring(0, habits[i].indexOf(','))])
-      name = habits[i].substring(0, habits[i].indexOf(','));
+            if (max) {
+                if (name.length > max) {
+                    max = name.length;
+                }
+            }
 
-      if (max) {
-        if (name.length > max) {
-          max = name.length;
+            if (!max) {
+                // the max is set for the first time
+                max = name.length;
+            }
+
+            for (let j = 0; j < habits.length; j++) {
+                if (habits[j].startsWith(name)) {
+                    if (times)
+                        times++; // increment times
+                } else {
+                    times = 1;
+                }
+
+                if (!dates[name]) {
+                    dates[name] = [];
+                }
+                dates[name] = dates[name].concat([new DayDate(habits[j].substring(habits[j].indexOf(',')))]);
+            }
         }
-      }
 
-      if (!max) {
-        // the max is set for the first time
-        max = name.length;
-      }
+        let streaks = [];
+        let prev = null;
+        let currentStreak = null;
 
-      for (var j = 0; j < habits.length; j++) {
-        if (habits[j].startsWith(name)) {
-          if (times) {
-            times++; // increment times
-          } else {
-            times = 1;
-          }
+        // create streaks
+        dates[name].forEach(dayDate => {
+            if (prev && prev.getNext().toString() === dayDate.toString().replace(',', '')) {
+                if (currentStreak) {
+                    currentStreak.setEndDate(dayDate);
+                } else {
+                    currentStreak = new Streak();
+                    currentStreak.setStartDate(dayDate);
+                    currentStreak.setEndDate(dayDate);
+                    streaks.push(currentStreak);
+                }
+            } else {
+                currentStreak = new Streak();
+                currentStreak.setStartDate(dayDate);
+                currentStreak.setEndDate(dayDate);
+                streaks.push(currentStreak);
+            }
+            // set previous date
+            prev = dayDate;
+        });
 
-          if (!dates[name]) {
-            dates[name] = [];
-          }
-          dates[name] = dates[name].concat([new DayDate(habits[j].substring(habits[j].indexOf(',')))]);
-        }
-      }
+        // filter out small streaks
 
-      streaks = [];
-      var prev = null;
-      var currentStreak = null;
+        // print best streak
+        streaks = new Streaks(streaks);
+        streaks.printStreaks(max, true, false);
 
-      // create streaks
-      dates[name].forEach(dayDate => {
-        if (prev && prev.getNext().toString() == dayDate.toString().replace(',', '')) {
-          if (currentStreak) {
-            currentStreak.setEndDate(dayDate);
-          } else {
-            currentStreak = new Streak();
-            currentStreak.setStartDate(dayDate);
-            currentStreak.setEndDate(dayDate);
-            streaks.push(currentStreak);
-          }
-        } else {
-          currentStreak = new Streak();
-          currentStreak.setStartDate(dayDate);
-          currentStreak.setEndDate(dayDate);
-          streaks.push(currentStreak);
-        }
-        // set previous date
-        prev = dayDate;
-      });
-
-      // filter out small streaks
-
-      // print best streak
-      streaks = new Streaks(streaks);
-      streaks.printStreaks(max, true, false);
-
-      times = 1;
+        times = 1;
     }
-  }
+}
 });
 
+// eslint-disable-next-line no-console
 console.log('\x1B[37;1;4mBest Streaks\x1B[0m');
--- reports/lastSevenDays.js
+++ reports/lastSevenDays.js
@@ -1,13 +1,10 @@
-var fs = require('fs');
-var DayDate = require('../src/DayDate');
-var Streak = require('../src/Streak');
+const fs = require('fs');
 
-var CounterBar = require('../src/CounterBar');
-var DaysBar = require('../src/DaysBar.js');
-var DayDate = require('../src/DayDate.js');
+const DaysBar = require('../src/DaysBar.js');
+const DayDate = require('../src/DayDate.js');
 
 
-var habits = [];
+let habits = [];
 
 console.log('\x1B[37;1;4mLast 7 Days\x1B[0m');
 
@@ -15,16 +12,16 @@ fs.readFile('./data.txt', 'utf-8', function(e, d) {
   habits = d.split(/\n/);
 
 
-  var done = [];
+  let done = [];
 
-  var dates = {};
+  const dates = {};
 
-  for (var i = 0; i < habits.length; i++) {
+  for (let i = 0; i < habits.length; i++) {
     if (!done.includes(habits[i].substring(0, habits[i].indexOf(',')))) {
       done = done.concat([habits[i].substring(0, habits[i].indexOf(','))])
-      name = habits[i].substring(0, habits[i].indexOf(','));
+      let name = habits[i].substring(0, habits[i].indexOf(','));
 
-      for (var j = 0; j < habits.length; j++) {
+      for (let j = 0; j < habits.length; j++) {
         if (habits[j].startsWith(name)) {
           if (!dates[name]) {
             dates[name] = [];
@@ -33,28 +30,27 @@ fs.readFile('./data.txt', 'utf-8', function(e, d) {
         }
       }
 
-      var howManyDays = 0;
-      var today = DayDate.today();
-      var sixDaysAgo = today.subtractDays(6);
-      var daysBar = new DaysBar(sixDaysAgo, today);
+      
+      const today = DayDate.today();
+      const sixDaysAgo = today.subtractDays(6);
+      const daysBar = new DaysBar(sixDaysAgo, today);
 
-      var currentDayDate = sixDaysAgo;
 
-      console.log('\x1B[37;1m' + name + '\x1B[0m: ');
+
+      console.log(`\x1B[37;1m + ${name} + \x1B[0m: `);
 
       while (currentDayDate.toString() <= today.toString()) {
-        if (dates[name].find(date => date.toString().replace(',', '') == currentDayDate.toString())) {
+        let currentDayDate = sixDaysAgo;
+        if (dates[name].find(date => date.toString().replace(',', '') === currentDayDate.toString())) {
           daysBar.addDayDate(currentDayDate, true);
-        } else {
-          if (currentDayDate.toString() != today.toString()) {
+        } else 
+        if (currentDayDate.toString() !== today.toString()) {
             daysBar.addDayDate(currentDayDate, false);
           }
-        }
+        
         currentDayDate = currentDayDate.getNext();
       }
-
       daysBar.printBar();
-
     }
   }
   // console.log('dates', dates);
